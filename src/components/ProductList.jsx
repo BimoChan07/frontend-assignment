ae632fe3a8d496a5d2b0cea8adfe85b930d0533e
BimoChan07 <bmochanb@gmail.com>
2023-07-21 16:32:12 +0545

    worked on product list

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,28 +1,15 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import { Link } from "react-router-dom";
 import Product from "./Products";
 import "../App.css";
 
-const ProductList = () => {
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    productFetch();
-  }, []);
-
-  const productFetch = async () => {
-    try {
-      const res = await fetch("http://localhost:8000/products");
-      const data = await res.json();
-      setProducts(data);
-      console.log(data);
-    } catch (error) {
-      console.error("Error while fetching products", error);
-    }
-  };
+const ProductList = ({ products }) => {
   return (
     <div className="product-list">
-      {products.map((product) => (
-        <Product key={product.id} product={product} />
+      {products.map((product, index) => (
+        <Link to={`/products/${product.id}`} key={product.id}>
+          <Product product={product} key={index} />
+        </Link>
       ))}
     </div>
   );
